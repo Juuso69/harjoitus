7329adde7922b2d6a876fd57447d0feea8462102
juuso69 <88772632+Juuso69@users.noreply.github.com>
2021-08-20 10:17:33 +0000

    2021/8/20

--- cps.js
+++ cps.js
@@ -1,32 +1,32 @@
 var started, resetTimeoutHandle, resetTimeout = 1000,
-    container = document.getElementById('container'),
-    counter = document.getElementById('counter'),
-    zone = document.getElementById('zone'),
-    clicks = 0;
+  container = document.getElementById('container'),
+  counter = document.getElementById('counter'),
+   zone = document.getElementById('zone'),
+   clicks = 0;
 
 zone.onseclect = zone.onselectstart = function() {
-    return false;
+  return false;
 };
 
 function clicksPerSecond(started, clicks) {
-    return clicks / ((new Date()) - started) * 1000;
+  return clicks / ((new Date()) - started) * 1000;
 }
 
 function count() {
-    clearTimeout(resetTimeoutHandle);
-    clicks++;
-    counter.innerText = clicksPerSecond(started, clicks);
-    resetTimeoutHandle = setTimeout(reset, resetTimeout);
-    return false;
+   clearTimeout(resetTimeoutHandle);
+   clicks++;
+   counter.innerText = clicksPerSecond(started, clicks);
+   resetTimeoutHandle = setTimeout(reset, resetTimeout);
+  return false;
 }
 
 function start() {
-    started = new Date();
-    clicks = 0;
-    counter.style.opacity = 1;
-    this.onmousedown = count;
-    this.onmousedown();
-    return false;
+  started = new Date();
+  clicks = 0;
+  counter.style.opacity = 1;
+  this.onmousedown = count;
+  this.onmousedown();
+  return false;
 }
 
 function reset() {
